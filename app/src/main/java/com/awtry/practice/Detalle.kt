3f7ca997b2277dd40776305ac42e533c7216bae1
Awtry <jorge_verah@outlook.es>
2021-09-11 14:09:59 -0500

    Intento de persistencia de datos

--- app/src/main/java/com/awtry/practice/Detalle.kt
+++ app/src/main/java/com/awtry/practice/Detalle.kt
@@ -1,13 +1,18 @@
 package com.awtry.practice
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.squareup.moshi.Moshi
+import java.lang.Exception
 
 class Detalle : AppCompatActivity() {
 
@@ -16,9 +21,21 @@ class Detalle : AppCompatActivity() {
     private var No_Foto: Int = 0
     private var Mi_Fav: Boolean = false
 
+
+    private val PREFS = "MY_PREFERENCES"
+    private val FAVORITE_USER = "FAVORITE_USER"
+    private lateinit var preferences: SharedPreferences
+    private val moshi = Moshi.Builder().build()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle)
+
+        preferences = getSharedPreferences(PREFS, Context.MODE_PRIVATE)
+        Le_Foto = savedInstanceState?.getParcelable(FAVORITE_USER) ?: INFO_FOTO()
+
+        Le_Foto = Es_Fav()
 
         Le_Foto = intent.getParcelableExtra("Mi_Fotito") ?: INFO_FOTO()
         No_Foto = intent.getIntExtra("No_Foto", 0)
@@ -26,9 +43,39 @@ class Detalle : AppCompatActivity() {
 
 
 
+
+
         initviews()
         Disparador_Boton_Detalle()
     }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        saveUserLikes()
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+
+        outState.run{
+            putParcelable(FAVORITE_USER, Le_Foto)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun saveUserLikes(FAV: INFO_FOTO? = null){
+        preferences.edit().putString(FAVORITE_USER, moshi.adapter(INFO_FOTO::class.java).toJson(FAV)).apply()
+    }
+
+    private fun Es_Fav() =
+        preferences.getString(FAVORITE_USER, null)?.let{
+            return@let try{
+                moshi.adapter(INFO_FOTO::class.java).fromJson(it)
+            }catch (e: Exception){
+                INFO_FOTO()
+            }
+        } ?: INFO_FOTO()
 
     //region iniciador
 
