faabf33799c8025032e9aa2beac2d775d868e87e
Awtry <jorge_verah@outlook.es>
2021-09-13 01:42:45 -0500

    Grafico de fragmentos
    Solo las cosas principales sin lógica

---
+++ app/src/main/java/com/awtry/practice/Frag_Detalle.kt
@@ -0,0 +1,12 @@
+package com.awtry.practice
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Frag_Detalle : Fragment() {
+
+}
---
+++ app/src/main/java/com/awtry/practice/Frag_Full_Foto.kt
@@ -0,0 +1,11 @@
+package com.awtry.practice
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Frag_Full_Foto : Fragment() {
+
+}
